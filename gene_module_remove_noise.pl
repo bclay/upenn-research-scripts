a1a577e8570085bc46c0696ac45060ce1d84699a
bclay <bclaypool@gmail.com>
2013-06-30 22:04:00 -0400

    new file gene_module...pl

---
+++ gene_module_remove_noise.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+#takes files with homologene and modularity value and removes noise
+#(i.e. homologenes that are the same across)
+#
+#inputs:
+#0: output from 
+
+
+use strict;
+use warnings;
+
+open(FILE, "<ARGV[0]") or die "error reading $ARGV[0]";
+
+
+#variable declarations
+
+
+
+#subrountine to do for each file
+
+#sub fileparser{ 
+
+
+
+
+
+close FILE;
