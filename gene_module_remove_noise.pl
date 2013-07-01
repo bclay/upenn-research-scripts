f2b21f4f018b1d2987e75c22ad11dd53052fa3d7
bclay <bclaypool@gmail.com>
2013-07-01 13:00:58 -0400

    added array of values per file and updated value for output

--- gene_module_remove_noise.pl
+++ gene_module_remove_noise.pl
@@ -18,6 +18,9 @@ my $count;
 my $line;
 my $val;
 my @lineSplit;
+my $lineNum;
+my $hgene2;
+my $fileCount;
 
 #loop to do for each file
 
@@ -27,27 +30,42 @@ foreach my $file (@FILES){
 	$data{$file} = <$fh>;
 }
 
+print "Hgene\tValue\tAcinar\tEndocr\tAdultBeta\tImmatBeta";
+
 sub getHundred{
 	while($_[$count] && $count < 100){
 		$val = 100 - $count;
 		@lineSplit = split('\t',$_[$count]);
-		if (exists $hundredLines{$lineSplit[0]}){
-			$hundredLines{$lineSplit[0]}+=$val;
+		$hgene2 = $lineSplit[0];
+
+		if (exists $hundredLines{$hgene2}){
+			${$hundredLines{$hgene2}}[0]+=($val+100);
+			#print "if\n";
 		}
 		else{
-			$hundredLines{$lineSplit[0]} = $val;
+			${$hundredLines{$hgene2}}[0] = $val;
+			#print "else\n";
+			$fileCount = scalar @FILES;
+			while ($fileCount > 0){
+				${$hundredLines{$hgene2}}[$fileCount] = 0;
+				$fileCount--;
+			}
 		}
+		${$hundredLines{$hgene2}}[$lineNum] = $val;
 		$count++;
 	}
 }
 
-
+$lineNum = 0;
 foreach my $key (keys %data){
 	$count = 0;
+	$lineNum++;
 	getHundred (split ('\n',$data{$key}));
 	print "\n"
 }
 
-foreach my $hgene (sort {$hundredLines{$b} <=> $hundredLines{$a}} keys %hundredLines){
-	print "$hgene\t$hundredLines{$hgene}\n";
+foreach my $hgene (sort {${$hundredLines{$b}}[0] <=> ${$hundredLines{$a}}[0]} keys %hundredLines){
+	print "$hgene\t";
+	print join("\t",@{$hundredLines{$hgene}});
+	print "\n";
 }
