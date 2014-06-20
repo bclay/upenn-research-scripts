0b18ace3ee7b8ac137a3d4f628ce73ff7bee5795
Brynn Claypoole <bclay@seas.upenn.edu>
2014-06-20 13:20:53 -0400

    study id in output

--- 2014/heatmap_microscope.pl
+++ 2014/heatmap_microscope.pl
@@ -139,7 +139,7 @@ foreach my $key2 (sort {$HoCats{$a} cmp $HoCats{$b}} keys %HoCats){
 		$line2 = $_;
 		@tokens2 = split (/\t/, $line2);
 		if ($tokens2[0] eq $key2){
-			print OUT1 "$tokens2[0]\t$HoCats{$key2}\t$tokens2[3]\t$tokens2[4]\n";
+			print OUT1 "$tokens2[1]\t$tokens2[3]\t$tokens2[4]\n";
 			$c = 0;
 			foreach(@{$HoProf{$key2}}){
 				print OUT1 "\t$_ : @{$HoHgenes{$key2}}[$c]\n";
