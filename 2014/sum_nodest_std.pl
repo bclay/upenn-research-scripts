5ab15951dbc73744db57a46927fea283c1235a57
bclay <bclaypool@gmail.com>
2014-07-15 17:53:01 -0400

    added std sum

---
+++ 2014/sum_nodest_std.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+#0:full file
+
+use strict;
+use warnings;
+use Statistics::Basic qw(:all nofill);
+
+open(FULL, "<$ARGV[0]") or die "error reading $ARGV[0]";
+
+my $line;
+my @tokens;
+my $count = 0;
+my $deg_sum = 0;
+my $apsp_sum = 0;
+my $cc_sum = 0;
+my @deg_arr;
+my @apsp_arr;
+my @cc_arr;
+
+while (<FULL>){
+	chomp;
+	$line = $_;
+	@tokens = split(/\t/, $line);
+	$count++;
+	if ($count>1){
+	$deg_sum = $deg_sum + $tokens[1];
+	$apsp_sum = $apsp_sum + $tokens[2];
+	$cc_sum = $cc_sum + $tokens[3];
+	push(@deg_arr, $tokens[1]);
+	push(@apsp_arr, $tokens[2]);
+	push(@cc_arr, $tokens[3]);
+}
+}
+
+my $deg_avg = $deg_sum/$count;
+my $apsp_avg = $apsp_sum/$count;
+my $cc_avg = $cc_sum/$count;
+my $deg_std = stddev(@deg_arr);
+my $apsp_std = stddev(@apsp_arr);
+my $cc_std = stddev(@cc_arr);
+
+
+print "$count\t$deg_avg\t$apsp_avg\t$cc_avg\n";
+print "$count\t$deg_std\t$apsp_std\t$cc_std\n";
+
+close FULL;
