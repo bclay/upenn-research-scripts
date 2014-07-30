fb8223b6b4c1f246eb0315e31e8448f0c9adfa68
bclay <bclaypool@gmail.com>
2014-07-30 14:35:23 -0400

    pagerank import works

--- 2014/pagerank.pl
+++ 2014/pagerank.pl
@@ -2,12 +2,18 @@
 #6/17/14
 #objective: output a list of x random genes
 #0: # of genes in list
+#1: table with ordered SE output: col 1 is gene ID, col 2 is rating, col 3 is normalized rating
 
 use strict;
 use warnings;
+use Graph::Centrality::Pagerank;
+use Data::Dump qw(dump);
 
-open(DATAFILE, "<$ARGV[1]") or die "error reading $ARGV[1]";
+open(OUT, ">$ARGV[0]") or die "error reading $ARGV[0]";
 
-#variable declaration
+my $r = Graph::Centrality::Pagerank->new();
+my $li = [[1,2],[3,4]];
+$r->getPagerankOfNodes (listOfEdges => $li);
 
-close DATAFILE;
+
+close OUT;
