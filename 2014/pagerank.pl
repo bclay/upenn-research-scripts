22c8b145d5c2dd28872b60968615403c03be8eea
bclay <bclaypool@gmail.com>
2014-08-01 13:20:57 -0400

    pagerank works

--- 2014/pagerank.pl
+++ 2014/pagerank.pl
@@ -10,15 +10,53 @@ use warnings;
 use Graph::Centrality::Pagerank;
 use Data::Dump qw(dump);
 
-#open(OUT, ">$ARGV[0]") or die "error reading $ARGV[0]";
+
+#declare variables
+my $li = [];
+my $nw = {};
+my $lineNW;
+my @tokensNW;
+my @temp;
+my @tokensDB;
+my $count =0;
+#read in the node weights
+open(NW, "<$ARGV[0]") or die "error reading $ARGV[0]";
+
+
+while(<NW>){
+	if ($count <=100){
+	chomp;
+	$lineNW = $_;
+	@tokensNW = split(/\t/, $lineNW);
+	#assign the normalized node weight to the homologene ID
+	$nw->{$tokensNW[0]} = $tokensNW[2];
+
+	#search the database for any entries containing this node
+	@temp = qx (grep -w "$tokensNW[0]" "$ARGV[1]");
+	foreach my $lineDB (@temp){
+		@tokensDB = split(/\t/, $lineDB);
+		push(@$li,[$tokensDB[0],$tokensDB[1]]);
+	}
+	$count++;
+	print "$tokensNW[0]\n";
+}}
+
+close NW;
+
+
+open(OUT, ">$ARGV[2]") or die "error reading $ARGV[2]";
+
 
 my $r = Graph::Centrality::Pagerank->new();
-my $li = [[1,2],[2,3]];
-my $nw = {};
-$nw->{1} = 0.9;
-$nw->{2} = 0.1;
-$nw->{3} = 0.1;
-dump $r->getPagerankOfNodes (listOfEdges => $li, nodeWeights => $nw);
 
 
-#close OUT;
+$r->getPagerankOfNodes (listOfEdges => $li, nodeWeights => $nw);
+my $hgene;
+for (keys %$r){
+	$hgene = $_;
+	print OUT "$hgene\t";
+	print OUT $r->{$hgene};
+	print OUT "\n";
+}
+
+close OUT;
